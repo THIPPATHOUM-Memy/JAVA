e6f4c3c4b7425bc2b56343475ba847220087dd42
memyt <memythippathoum@gmail.com>
2022-10-05 22:54:15 +0700

    Bai ke thua IEmployee

--- IEmployee/Entry.java
+++ IEmployee/Entry.java
@@ -1,11 +1,11 @@
 package IEmployee;
 public class Entry {
     public static void main(String[] args) {
-        IEmployee employee1 = new PartTimeEmployee("thavatxay philavan", 45000, 7);
+        IEmployee employee1 = new PartTimeEmployee("latdavone", 45000, 7);
         System.out.println("Name: " + employee1.getName());
         System.out.println("Salary per day: " + employee1.calculateSalary());
 
-        IEmployee employee2 = new FullTimeEmployee("nutthaphon kongmany", 65000);
+        IEmployee employee2 = new FullTimeEmployee("thanomchit", 65000);
         System.out.println("Name: " + employee2.getName());
         System.out.println("Salary per day: " + employee2.calculateSalary());
     }
